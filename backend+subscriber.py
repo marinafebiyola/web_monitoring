f24c673f428f56b3b3eaccf83b538176671b1098
marinafebiyola <marinafebiyol@gmail.com>
2024-10-01 17:04:10 +0700

    Update backend+subscriber.py

--- backend+subscriber.py
+++ backend+subscriber.py
@@ -6,12 +6,12 @@ from kki_2024.msg import lokalisasi
 from datetime import datetime
 
 
-nilai_x = nilai_y = latt = lon = knot = km_per_hours = cog = 0
+nilai_x = nilai_y = latt = lon = knot = km_per_hours = cog = yaw = 0
 app = Flask(__name__)
 
 # ROS callback function
 def lokalisasi_callback(data):
-    global nilai_x, nilai_y, latt, lon, knot, km_per_hours, cog
+    global nilai_x, nilai_y, latt, lon, knot, km_per_hours, cog, yaw
     nilai_x = data.nilai_x
     nilai_y = data.nilai_y
     latt = data.latt
@@ -19,7 +19,8 @@ def lokalisasi_callback(data):
     knot = data.knot
     km_per_hours = data.km_per_hours
     cog = data.cog
-    print(f"x: {nilai_x}, y: {nilai_y}, lat: {latt}, lon: {lon}, knot1: {knot}, knot2: {km_per_hours}, cog: {cog}")
+    yaw = data.yaw
+    print(f"x: {nilai_x}, y: {nilai_y}, lat: {latt}, lon: {lon}, knot1: {knot}, knot2: {km_per_hours}, cog: {cog}, yaw: {yaw}")
 
 def monitoring():
     rospy.init_node('monitoring', anonymous=True)
@@ -46,6 +47,7 @@ def get_data_receive():
         'sog_knot': knot,
         'sog_kmh': km_per_hours,
         'cog': cog,
+        'yaw' : yaw
     }
     return jsonify(data)
 
